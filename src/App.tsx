83839417c1d68a5a0e6ed7dd7a72bb7fb3809013
qjd-yyds <qjd1774@163.com>
2022-08-25 08:53:29 +0800

    feat: file上传

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import MenuExample from "./components/Menu/example"
-import ButtonExample from "./components/Button/example"
-import TransitionExample from "./components/Transition/example"
-import AutoCompleteExample from "./components/AutoComplete/example"
-import InputExample from "./components/Input/example"
+// import MenuExample from "./components/Menu/example"
+// import ButtonExample from "./components/Button/example"
+// import TransitionExample from "./components/Transition/example"
+// import AutoCompleteExample from "./components/AutoComplete/example"
+// import InputExample from "./components/Input/example"
+import {ChangeEvent, useEffect, useState} from "react";
+import axios from "axios";
+
 function App() {
+    const [title, setTitle] = useState("dass")
+    const hanldeOnchange = (e: ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files
+        if (files) {
+            const file = files[0]
+            const formData = new FormData()
+            formData.append(file.name, file)
+            console.log(formData)
+            axios.post("https://jsonplaceholder.typicode.com/posts", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            }).then(res => {
+                console.log(res)
+            })
+        }
+    }
+
     return (
         <div className="App">
-            <AutoCompleteExample/>
+            <h2>{title}</h2>
+            <input type="file" name="myfile" onChange={hanldeOnchange}/>
+            {/*<AutoCompleteExample/>*/}
             {/*<ButtonExample/>*/}
             {/*<hr/>*/}
             {/*<MenuExample/>*/}
---
+++ src/components/Upload/upload.tsx
@@ -0,0 +1,14 @@
+const Upload = () => {
+    return <div>123</div>
+}
+// <Upload
+// action="https://upload"
+// beforeUpload={}
+// onProgress={}
+// onChange={}
+// onSuccess={}
+// onError={}
+// onRemove={}
+// >
+// </Upload>
+export default Upload
